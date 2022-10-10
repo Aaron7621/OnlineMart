bca2dc7d22b06e15af7c186e7c93c2a7f07e0934
Xie Jing <xj1224999@163.com>
2022-10-10 19:13:59 +0800

    test

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
-# testestetetettaaasaada
-
-# aaron 18:52 test
-# adddada
-
-def testss():
-    return 0
-# gffdfggmjkojmko1
-
-# abtd
-
-
-# dfss啊发发
+#111
+#111
